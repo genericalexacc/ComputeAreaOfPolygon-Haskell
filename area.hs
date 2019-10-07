05d55ef36e4664ff0c60dd7fa45073ba733dad29
Alexander Shevchenko <alexander_shev@mail.ru>
2019-10-07 15:37:39 -0400

    Create area.hs

---
+++ area.hs
@@ -0,0 +1,25 @@
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+import Control.Monad
+import Data.List
+import Data.List.Split
+
+intsToTuple :: [String] -> (Int, Int)
+intsToTuple [a, b] = (read a :: Int, read b :: Int)
+
+getArea :: [(Int, Int)] -> Int
+getArea [x] = 0
+getArea (x:xs) = currentDistance + getArea xs
+    where
+        currentDistance = fst(x) * snd(head xs) - snd(x) * fst(head xs)
+
+main :: IO()
+main = do
+    q <- readLn :: IO Int
+    coordinates <- forM [1..q] $ (\q_itr -> do
+        s <- getLine
+        return (intsToTuple (splitOn " " s))
+        )
+    putStrLn(show (fromIntegral (area coordinates) / 2))
+    where 
+        area coordinates = getArea coordinates + getArea [last coordinates, head coordinates]
+
